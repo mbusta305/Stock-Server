a779991b27da47e9e75b3c9091121a072dc61606
Michael Busta <busta2356@gmail.com>
2016-04-19 19:00:35 -0400

    Any Stock

--- server.rb
+++ server.rb
@@ -9,6 +9,8 @@ require 'nokogiri'
 
 get '/' do
 
+stock = params[:stock]
+
   url = "https://www.google.com/finance?cid=22144"
 
   response = HTTParty.get url
@@ -20,6 +22,36 @@ get '/' do
   stock_price = matching_span_element.content
 
   "The current stock price of AAPL is $ #{stock_price}."
+
+end
+
+get '/error' do
+  'boom'
+end
+
+
+require 'sinatra'
+
+require 'httparty'
+
+require 'json'
+
+require 'nokogiri'
+
+
+get '/' do
+
+stock = params[:stock]
+
+url = "http://finance.yahoo.com/q?s=#{stock}"
+ response = HTTParty.get url
+
+
+ dom = Nokogiri::HTML(response.body)
+
+ my_span = dom.xpath("//span[@id='yfs_l84_#{stock}']").first
+
+"The Stock you Chose is currently at $#{my_span}"
 
 end
 
