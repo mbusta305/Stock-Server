2af338f1ee5a5093127211d0eb69c20fb67a92c6
Michael Busta <busta2356@gmail.com>
2016-04-19 19:03:19 -0400

    Fixed Errors

--- server.rb
+++ server.rb
@@ -11,38 +11,6 @@ get '/' do
 
 stock = params[:stock]
 
-  url = "https://www.google.com/finance?cid=22144"
-
-  response = HTTParty.get url
-
-  dom = Nokogiri::HTML(response.body)
-
-  list_of_matching_spans = dom.xpath("//span[@id='ref_22144_l']")
-  matching_span_element = list_of_matching_spans.first
-  stock_price = matching_span_element.content
-
-  "The current stock price of AAPL is $ #{stock_price}."
-
-end
-
-get '/error' do
-  'boom'
-end
-
-
-require 'sinatra'
-
-require 'httparty'
-
-require 'json'
-
-require 'nokogiri'
-
-
-get '/' do
-
-stock = params[:stock]
-
 url = "http://finance.yahoo.com/q?s=#{stock}"
  response = HTTParty.get url
 
